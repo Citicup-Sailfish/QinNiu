97052d2be20f4236535ad48826c6f060ee3d5d6a
linzibo <673418747@qq.com>
2015-08-15 10:44:30 +0800

    upload price  PriceTabSHSZActivity.java

---
+++ qinNiuClient/app/src/main/java/com/qinniuclient/price/expandable/PriceItemGroup.java
@@ -0,0 +1,14 @@
+package com.qinniuclient.price.expandable;
+
+public class PriceItemGroup {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ qinNiuClient/app/src/main/java/com/qinniuclient/price/expandable/PriceItemPeople.java
@@ -0,0 +1,33 @@
+package com.qinniuclient.price.expandable;
+
+public class PriceItemPeople {
+
+    private String name;
+    private String increase;
+    private String percentage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getincrease() {
+        return increase;
+    }
+
+    public void setincrease(String increase) {
+        this.increase = increase;
+    }
+
+    public String getpercentage() {
+        return percentage;
+    }
+
+    public void setpercentage(String percentage) {
+        this.percentage = percentage;
+    }
+
+}
